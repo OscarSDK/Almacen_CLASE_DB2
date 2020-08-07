bda97d765065ef08046507573ae05faa83ee9573
JOSE <JOSE@LAPTO-HP>
2020-08-07 16:25:49 -0500

    escrimbierndo mi nomnre

--- ProyectoDB2/src/proyectodb2/ProyectoDB2.java
+++ ProyectoDB2/src/proyectodb2/ProyectoDB2.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectodb2;
 
-/**
- *
- * @author user
- */
+
 public class ProyectoDB2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         //----------- OSCAR-,----------------------
         String oscar="oscar";
@@ -53,7 +44,9 @@ public class ProyectoDB2 {
         String Santiago="Santiago";
         System.out.println("Mi nombre es: "+Santiago);
         
-        
+        //***********JOSE LUIS CONDO CHOQQUE*******************
+        String Jose_Luis=" JOSE LUIS CONDO CHOQQUE";
+        System.out.println("Mi nombre es"+ Jose_Luis);
     }
     
 }
